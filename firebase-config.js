da1e0a9982e3cde00af4fbb65896776a20a56d2e
mennin-gh <lutheransluther20@gmail.com>
2026-01-30 12:07:55 +0000

    update

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDk2162LJTRPRKtNPjwK1wdIuXGXG6JScA",
+  authDomain: "firechat-pro-52ed5.firebaseapp.com",
+  projectId: "firechat-pro-52ed5",
+  storageBucket: "firechat-pro-52ed5.firebasestorage.app",
+  messagingSenderId: "828493856491",
+  appId: "1:828493856491:web:8e4fb3346512d9179988ae"
+};
